a272816f9c2f5a1332b3efd0af973b9072b148f5
SheetKey <wveatchw@gmail.com>
2021-11-05 15:27:17 -0400

    syntax fix

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     nixpkgs.url = "nixpkgs/nixos-unstable";
 
     home-manager.url = "github:nix-community/home-manager/master";
-    home-manager.inputs.nixpkgs.follows = "mixpkgs";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
   };
 
