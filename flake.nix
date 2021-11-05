ca70308d404563df81166a7bdc56fb446a5f2e9e
SheetKey <wveatchw@gmail.com>
2021-11-05 15:24:05 -0400

    Added flake

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "My system config.";
+
+  inputs = {
+
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+
+    home-manager.url = "github:nix-community/home-manager/master";
+    home-manager.inputs.nixpkgs.follows = "mixpkgs";
+
+  };
+
+  outputs = inputs@{ self, nixpkgs, home-manager, ... }: 
+
+  let 
+
+    system = "x86_64-linux";
+
+    pkgs = import nixpkgs {
+      
+      inherit system;
+
+      config = {
+	allowUnfree = true;
+      };
+
+    };
+
+    lib = nixpkgs.lib;
+
+  in {
+
+    nixosConfigurations = {
+      ### Nixos hostname
+      nixos = lib.nixosSystem {
+	inherit system;
+
+        modules = [
+          ./configuration.nix
+	];
+      };
+
+    };
+
+  };
+}
