ed66995483d528ea4a0c242318a6c673ae0dd454
Janevbox <jane@vbox.com>
2021-11-14 14:39:13 +0000

    removed boot settings from main config

--- configuration.nix
+++ configuration.nix
@@ -22,15 +22,15 @@
     '';
   };
 
-
+############## ADD BOOT SETTINGS IN HOST SPECIFIC CONFIG#########################
   # Use the GRUB 2 boot loader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.version = 2;
+  # boot.loader.grub.enable = true;
+  # boot.loader.grub.version = 2;
   # boot.loader.grub.efiSupport = true;
   # boot.loader.grub.efiInstallAsRemovable = true;
   # boot.loader.efi.efiSysMountPoint = "/boot/efi";
   # Define on which hard drive you want to install Grub.
-  boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
+  # boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
 
 
   # Set up locales.
