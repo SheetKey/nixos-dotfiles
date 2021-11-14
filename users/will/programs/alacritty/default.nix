8bf6600c3434aff306c967d1289056b17a20fd1f
will <will@vbox.com>
2021-11-14 14:20:05 -0500

    type correction

--- users/will/programs/alacritty/default.nix
+++ users/will/programs/alacritty/default.nix
@@ -30,38 +30,38 @@
       colors = {
         # Default colors
 	primary = {
-	  background = '#002b36';
-	  foreground = '#839496';
+	  background = "#002b36";
+	  foreground = "#839496";
 	};
 
 	# Cursor colors
 	cursor = {
-	  text = '#002b36';
-	  cursor = '#839496';
+	  text = "#002b36";
+	  cursor = "#839496";
 	};
 
 	# Normal colors
 	normal = {
-	  black = '#073642';
-	  red = '#dc322f';
-	  green = '#859900';
-	  yellow = '#b58900';
-	  blue = '#268bd2';
-	  magenta = '#d33682';
-	  cyan = '#2aa198';
-	  white = '#eee8d5';
+	  black = "#073642";
+	  red = "#dc322f";
+	  green = "#859900";
+	  yellow = "#b58900";
+	  blue = "#268bd2";
+	  magenta = "#d33682";
+	  cyan = "#2aa198";
+	  white = "#eee8d5";
 	};
 
 	# Bright colors
 	bright = {
-	  black = '#586e75';
-	  red = '#cb4b16';
-	  green = '#586e75';
-	  yellow = '#657b83';
-	  blue = '#839496';
-	  magenta = '#6c71c4';
-	  cyan = '#93a1a1';
-	  white = '#fdf6e3';
+	  black = "#586e75";
+	  red = "#cb4b16";
+	  green = "#586e75";
+	  yellow = "#657b83";
+	  blue = "#839496";
+	  magenta = "#6c71c4";
+	  cyan = "#93a1a1";
+	  white = "#fdf6e3";
 	};
 
       };
