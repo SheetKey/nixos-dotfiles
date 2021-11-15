374a23935760311c607d14ac7574357fa0a22900
will <will@vbox.com>
2021-11-15 16:12:25 -0500

    fixes

--- users/will/programs/xmobar/trayer-padding-icons.sh
+++ users/will/programs/xmobar/trayer-padding-icons.sh
@@ -1,19 +1,29 @@
 #!/bin/sh
+# Copied from https://github.com/jaor/xmobar/issues/239#issuecomment-233206552
+# Detects the width of running trayer-srg window (xprop name 'panel')
+# and creates an XPM icon of that width, 1px height, and transparent.
+# Outputs an <icon>-tag for use in xmobar to display the generated
+# XPM icon.
+#
+# Run script from xmobar:
+# `Run Com "/where/ever/trayer-padding-icon.sh" [] "trayerpad" 10`
+# and use `%trayerpad%` in your template.
 
-# Funtion to create transparent Wx1 px XPM icon
+
+# Function to create a transparent Wx1 px XPM icon
 create_xpm_icon () {
-	timestamp=$(date)
-	pixels=$(for i in `seq $1`; do echo -n "."; done)
+    timestamp=$(date)
+    pixels=$(for i in `seq $1`; do echo -n "."; done)
 
-	cat << EOF > "$2"
-
+    cat << EOF > "$2"
 /* XPM *
-static char * trayer_pad_xp[] = {
-/* This XPM icon is used for padding in xmobar */
-/* leave room for trayer-srg. It is dynamically */
-/* updated by trayer-padding-icon.sh which is run *//* by xmobar.
+static char * trayer_pad_xpm[] = {
+/* This XPM icon is used for padding in xmobar to */
+/* leave room for trayer-srg. It is dynamically   */
+/* updated by by trayer-padding-icon.sh which is run  */
+/* by xmobar.                                     */
 /* Created: ${timestamp} */
-/* <w/cols> <h/rows> <colors> <char per pixel> */
+/* <w/cols>  <h/rows>  <colors>  <chars per pixel> */
 "$1 1 1 1",
 /* Colors (none: transparent) */
 ". c none",
@@ -24,15 +34,16 @@ EOF
 }
 
 # Width of the trayer window
-width=$(xpop -name panel | grep 'prgram specified minimum size' | cut -d ' ' -f 5)
+width=$(xprop -name panel | grep 'program specified minimum size' | cut -d ' ' -f 5)
 
 # Icon file name
 iconfile="/tmp/trayer-padding-${width}px.xpm"
 
-# If the desired icon does not exists create it
+# If the desired icon does not exist create it
 if [ ! -f $iconfile ]; then
-	create_xpm_icon $width $iconfile
+    create_xpm_icon $width $iconfile
 fi
 
 # Output the icon tag for xmobar
 echo "<icon=${iconfile}/>"
+
