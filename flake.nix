28a88bbd1900f3e99917897517cc4de19d1053a1
Will <wveatchw@gmail.com>
2021-12-04 22:29:00 +0000

    syntax fix

--- flake.nix
+++ flake.nix
@@ -60,7 +60,7 @@
 	  home-manager.nixosModules.home-manager {
 	    home-manager.useGlobalPkgs = true;
 	    home-manager.useUserPackages = true;
-	    home-manager.user.will = import ./users/will/home.nix;
+	    home-manager.users.will = import ./users/will/home.nix;
 	    nixpkgs.overlays = [
 	      nur.overlay emacs-overlay.overlay
 	    ];
