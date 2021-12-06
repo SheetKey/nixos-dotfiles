3ca9f483c5134e16c0bc091c865bc653b1887595
Will <wveatchw@gmail.com>
2021-12-06 13:43:28 -0500

    emacs.init overlay

--- users/will/programs/emacs/default.nix
+++ users/will/programs/emacs/default.nix
@@ -1,14 +1,6 @@
-{ pkgs, nur, nixpkgs, ... }:
+{ pkgs, ... }:
 
-let 
-
-  nur-no-pkgs = import nur {
-    nurpkgs = import nixpkgs { system = "x86_64-linux"; };
-  };
-  
-in
 {
-  imports = [ nur-no-pkgs.repos.rycee.hmModules.emacs-init ];
 
   programs.emacs = {
     enable = true;
