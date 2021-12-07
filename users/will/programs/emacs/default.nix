74dc6b51157b4edd69203834a93968b350723dc7
Will <wveatchw@gmail.com>
2021-12-07 09:51:03 -0500

    emacs ui changes

--- users/will/programs/emacs/default.nix
+++ users/will/programs/emacs/default.nix
@@ -30,7 +30,7 @@
         (setq visible-bell t)
 
         ;; Font
-        (set-face-attribute 'default nit :font "Ubuntu Nerd Font" :height 12)
+        (set-face-attribute 'default nil :font "Ubuntu Nerd Font" :height 12)
       '';
 
       # extra packages for emacs
