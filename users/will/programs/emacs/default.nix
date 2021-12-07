34c0d8042fe19d0c747ef8b4ffc385faa54f3db7
Will <wveatchw@gmail.com>
2021-12-07 09:30:08 -0500

    disable startup message

--- users/will/programs/emacs/default.nix
+++ users/will/programs/emacs/default.nix
@@ -11,6 +11,21 @@
       recommendedGcSettings = true;
       usePackageVerbose = false;
 
+      # early-init.el file
+      earlyInit = ''
+
+      '';
+
+      # beginning of init.el
+      prelude = ''
+        ;; disable startup message
+        (setq inhibit-startup-message t)
+      '';
+
+      # extra packages for emacs
+      usePackage = {
+
+      };
 
     };
   };
