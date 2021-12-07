446e80ec8540465823412abb8d992eac9eaf8300
Will <wveatchw@gmail.com>
2021-12-07 18:16:18 -0500

    emacs ivy

--- users/will/programs/emacs/default.nix
+++ users/will/programs/emacs/default.nix
@@ -51,6 +51,21 @@
           config = ''
             (ivy-mode 1)
           '';
+          extraConfig = ''
+            :bind ( :map ivy-minibuffer-map
+                    ("TAB" . ivy-alt-done)
+                    ("C-l" . ivy-alt-done)
+                    ("C-j" . ivy-next-line)
+                    ("C-k" . ivy-previous-line)
+                    :map ivy-switch-buffer-map
+                    ("C-k" . ivy-previous-line)
+                    ("C-l" . ivy-done)
+                    ("C-d" . ivy-switch-buffer-kill)
+                    :map ivy-reverse-i-search-map
+                    ("C-k" . ivy-previous-line)
+                    ("C-d" . ivy-reverse-i-search-kill)
+                  )
+          '';
         };
 
       };
