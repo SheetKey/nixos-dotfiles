31dcf4107fd0af2cbc842cadcd0ac3788f89ca34
Will <wveatchw@gmail.com>
2021-12-08 18:36:48 -0500

    helpful

--- users/will/programs/emacs/default.nix
+++ users/will/programs/emacs/default.nix
@@ -126,9 +126,16 @@
         # Helpful: a better help tool
         helpful = {
           enable = true;
-          bind = {
-            [remap describe-function] = "counsel-describe-function"; 
-          };
+          extraConfig = ''
+            :custom
+            (counsel-describe-function-function #'helpful-callable)
+            (counsel-describe-variable-function #'helpful-variable)
+            :bind
+            ([remap describe-function] . counsel-describe-function)
+            ([remap describe-command] . helpful-command)
+            ([remap describe-variable] . counsel-describe-variable)
+            ([remap describe-key] . helpful-key)
+          '';
         };
 
       };
