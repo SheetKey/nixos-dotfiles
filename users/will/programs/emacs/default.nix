393818d2575ac7792e327cc0ed21bb80bfae657e
Will <wveatchw@gmail.com>
2021-12-08 18:57:24 -0500

    helpful

--- users/will/programs/emacs/default.nix
+++ users/will/programs/emacs/default.nix
@@ -122,18 +122,21 @@
             (marginalia-annotators '(marginalia-annotators-heavy marginalia-annotators-light nil))
           '';
         };
+        # Consult with vertico
+        consult = {
+          enable = true;
+          
+        };
 
         # Helpful: a better help tool
         helpful = {
           enable = true;
           extraConfig = ''
-            :custom
-            (counsel-describe-function-function #'helpful-callable)
-            (counsel-describe-variable-function #'helpful-variable)
             :bind
-            ([remap describe-function] . counsel-describe-function)
+            ([remap describe-function] . helpful-function)
+            ([remap describe-symbol] . helpful-symbol)
             ([remap describe-command] . helpful-command)
-            ([remap describe-variable] . counsel-describe-variable)
+            ([remap describe-variable] . helpful-variable)
             ([remap describe-key] . helpful-key)
           '';
         };
