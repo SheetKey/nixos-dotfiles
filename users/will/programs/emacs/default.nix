c1a819386a8c99cbccb979149c7067b726416a59
Will <wveatchw@gmail.com>
2021-12-09 16:34:30 -0500

    doom themes

--- users/will/programs/emacs/default.nix
+++ users/will/programs/emacs/default.nix
@@ -68,12 +68,11 @@
                   doom-themes-enable-italic t)
             (load-theme 'doom-solarized-dark t)
 
-            ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;NEEDS TO BE FIXED
             ;; Enable flashing mode-line on errors
-            ;;(doom-themes-visual-bell-config)
+            (doom-themes-visual-bell-config t)
 
             ;; For org-mode
-            (doom-themes-org-config)
+            (doom-themes-org-config t)
           '';
         };
 
