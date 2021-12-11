24a8534591d358ea146e96668b83b65dc1c99c02
Will <wveatchw@gmail.com>
2021-12-10 20:27:31 -0500

    evil mode

--- users/will/programs/emacs/default.nix
+++ users/will/programs/emacs/default.nix
@@ -182,13 +182,13 @@
         # Evil mode
         evil = {
           enable = true;
-          init = ''
-            (setq evil-want-integration t)
-            (setq evil-want-keybinding nil)
-            (setq evil-want-C-u-scroll t)
-            (setq evil-want-C-i-jump nil)
-          '';
-          hook = [ "(evil-mode . will/evil-hook)" ];
+        #  init = ''
+        #    (setq evil-want-integration t)
+        #    (setq evil-want-keybinding nil)
+        #    (setq evil-want-C-u-scroll t)
+        #    (setq evil-want-C-i-jump nil)
+        #  '';
+        #  hook = [ "(evil-mode . will/evil-hook)" ];
           config = ''
             (evil-mode 1)
             (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
