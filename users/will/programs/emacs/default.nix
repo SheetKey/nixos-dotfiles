bceae83b3760666431a38f0e7c45a314ee984464
Will <wveatchw@gmail.com>
2021-12-10 22:24:38 -0500

    font size

--- users/will/programs/emacs/default.nix
+++ users/will/programs/emacs/default.nix
@@ -43,7 +43,7 @@
         (setq visible-bell t)
 
         ;; Font
-        (set-face-attribute 'default nil :font "FiraCode Nerd Font Mono" :height 150)
+        (set-face-attribute 'default nil :font "FiraCode Nerd Font Mono" :height 130)
 
         ;;;;;;;;;;; KEYBINDS
         ;; Make ESC quit prompts
