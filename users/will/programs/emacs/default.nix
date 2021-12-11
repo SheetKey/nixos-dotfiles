fe2dc757895d79078eef33154c6efd2de3404c13
Will <wveatchw@gmail.com>
2021-12-11 16:07:08 -0500

    Org heading font size

--- users/will/programs/emacs/default.nix
+++ users/will/programs/emacs/default.nix
@@ -70,6 +70,18 @@
             (auto-fill-mode 0)
             (visual-line-mode 1)
             (setq evil-auto-indent nill))
+            (setq evil-auto-indent nil))
+
+          ;;;;;;;;;; Org-mode heading sizes function
+          (dolist (face '((org-level-1 . 1.2)
+                          (org-level-2 . 1.1)
+                          (org-level-3 . 1.05)
+                          (org-level-4 . 1.0)
+                          (org-level-5 . 1.1)
+                          (org-level-6 . 1.1)
+                          (org-level-7 . 1.1)
+                          (org-level-8 . 1.1)))
+                  (set-face-attribute (car face) nil :font "Cantarell" :weight 'regular :height (cdr face)))
       '';
 
       # extra packages for emacs
