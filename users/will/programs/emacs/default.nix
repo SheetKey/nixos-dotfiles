6d6ef5b737e737b620fae95e83a708b90b794008
Will <wveatchw@gmail.com>
2021-12-19 14:43:48 -0500

    Ubuntu font

--- users/will/programs/emacs/default.nix
+++ users/will/programs/emacs/default.nix
@@ -81,7 +81,7 @@
                             (org-level-6 . 1.1)
                             (org-level-7 . 1.1)
                             (org-level-8 . 1.1)))
-                    (set-face-attribute (car face) nil :font "Inconsolata Nerd Font" :weight 'regular :height (cdr face))))
+                    (set-face-attribute (car face) nil :font "Ubuntu Nerd Font" :weight 'regular :height (cdr face))))
       '';
 
       # extra packages for emacs
