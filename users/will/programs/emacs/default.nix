60d9b009de61338d61630f27cc35eff61161e45d
Will <wveatchw@gmail.com>
2021-12-26 16:15:56 -0500

    Org agenda log mode

--- users/will/programs/emacs/default.nix
+++ users/will/programs/emacs/default.nix
@@ -294,6 +294,10 @@
             (setq org-ellipsis " ▾"
                   org-hide-emphasis-markers t)
 
+            (setq org-agenda-start-with-log-mode t)
+            (setq org-log-done 'time)
+            (setq org-log-into-drawer t)
+
             (setq org-agenda-files
                   '("~/Documents/Org/Tasks.org"))
 
