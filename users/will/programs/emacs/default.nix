ab2f45e6c469aed5311d846a59c77f33e98e9714
Will <wveatchw@gmail.com>
2022-01-07 15:50:16 -0500

    Dired config

--- users/will/programs/emacs/default.nix
+++ users/will/programs/emacs/default.nix
@@ -452,6 +452,18 @@
           '';
         };
 
+        ### FILE MANAGEMENT
+        # Dired
+        dired = {
+          enable = true;
+          command = [ "dired" "dired-jump" ];
+          config = ''
+            (evil-collection-define-key 'normal 'dired-mode-map
+              "h" 'dired-up-directory
+              "l" 'dired-find-file)
+          '';
+        };
+
       };
 
     };
