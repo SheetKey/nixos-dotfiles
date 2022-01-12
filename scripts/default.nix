c650719be5d2f7fb2561a736066546600172b257
Will <wveatchw@gmail.com>
2022-01-12 18:30:28 -0500

    nvidia-offload script

--- scripts/default.nix
+++ scripts/default.nix
@@ -3,6 +3,14 @@
 { pkgs, ... }:
 
 let
+  nvidia-offload = pkgs.writeShellScriptBin "nvidia-offload" ''
+    export __NV_PRIME_RENDER_OFFLOAD=1
+    export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+    export __GLX_VENDOR_LIBRARY_NAME=nvidia
+    export __VK_LAYER_NV_optimus=NVIDIA_only
+    exec -a "$0" "$@"
+  '';
+
   getKernelScript = pkgs.writeShellScriptBin "getKernelScript" ''
     kern="$(uname -r)"
     echo -e "$kern "
@@ -62,5 +70,6 @@ in {
   environment.systemPackages = [ 
     getKernelScript
     trayer-padding-icon
+    nvidia-offload
   ];
 }
