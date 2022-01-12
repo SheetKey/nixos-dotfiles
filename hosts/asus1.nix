d819338004a8a3212cd866366047319e2564eddf
Will <wveatchw@gmail.com>
2022-01-12 18:30:47 -0500

    use nvidia drivers

--- hosts/asus1.nix
+++ hosts/asus1.nix
@@ -18,6 +18,21 @@
 
   # hardware.enableRedistributableFirmware = true;
 
+  #################### NVIDIA
+
+    services.xserver.videoDrivers = [ "nvidia" ];
+    hardware.nvidia.prime = {
+      offload.enable = true;
+    
+      # Bus ID of the Intel GPU. You can find it using lspci, either under 3D or VGA
+      intelBusId = "PCI:0:2:0";
+    
+      # Bus ID of the NVIDIA GPU. You can find it using lspci, either under 3D or VGA
+      nvidiaBusId = "PCI:1:0:0";
+    };
+
+  ###########################
+
   # COPIED FROM GENERATED configuration.nix
     system.stateVersion = "21.11";
 
