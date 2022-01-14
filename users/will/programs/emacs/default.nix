2f8b16efa99d3b6874f963866f20bd6c5b5df441
Will <wveatchw@gmail.com>
2022-01-13 19:59:12 -0500

    mashScipt

--- users/will/programs/emacs/default.nix
+++ users/will/programs/emacs/default.nix
@@ -338,7 +338,7 @@
                 )
             )
 
-            (setq org-babel-mathematica-command "~/dotfiles/nixos-dotfiles/scripts/mash")
+            (setq org-babel-mathematica-command "mashScript")
 
             (will/org-font-setup)
           '';
@@ -409,7 +409,7 @@
         wolfram-mode = {
           enable = true;
           config = ''
-            (setq mathematica-command-line "~/dotfiles/nixos-dotfiles/scripts/mash")
+            (setq mathematica-command-line "mashScript")
             (add-to-list 'org-src-lang-modes '("mathematica" . wolfram))
           '';
         };
