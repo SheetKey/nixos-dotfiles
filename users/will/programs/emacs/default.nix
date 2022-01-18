5f0ec78745d4c4fcc7eaad644f5f49df48e6513b
Will <wveatchw@gmail.com>
2022-01-18 16:01:27 -0500

    cleaning up

--- users/will/programs/emacs/default.nix
+++ users/will/programs/emacs/default.nix
@@ -94,13 +94,6 @@
                             (org-level-7 . 1.3)
                             (org-level-8 . 1.3)))
                     (set-face-attribute (car face) nil :font "Hack Nerd Font Mono" :weight 'bold :height (cdr face)))
-            ; (set-face-attribute 'org-block nil :foreground nil :inherit 'fixed-pitch)
-            ; (set-face-attribute 'org-code nil :inherit '(shadow fixed-pitch))
-            ; (set-face-attribute 'org-table nil :inherit '(shadow fixed-pitch))
-            ; (set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
-            ; (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
-            ; (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
-            ; (set-face-attribute 'org-checkbox nil :inherit 'fixed-pitch)
           )
 
       '';
