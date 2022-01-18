70b7db3424534555369c78e38f3bbba29889b9b5
Will <wveatchw@gmail.com>
2022-01-18 16:01:03 -0500

    font size

--- users/will/programs/emacs/default.nix
+++ users/will/programs/emacs/default.nix
@@ -55,7 +55,7 @@
         ;; Font
         (set-face-attribute 'default nil :font "FiraCode Nerd Font Mono" :height 110)
         (set-face-attribute 'fixed-pitch nil :font "FiraCode Nerd Font Mono" :height 110)
-        (set-face-attribute 'variable-pitch nil :font "TeX Gyre Pagella" :height 110 :weight 'regular)
+        (set-face-attribute 'variable-pitch nil :font "TeX Gyre Pagella" :height 150 :weight 'regular)
 
         ;;;;;;;;;;; KEYBINDS
         ;; Make ESC quit prompts
