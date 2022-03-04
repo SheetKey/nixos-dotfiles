94c0b1ed5530aae2d233a67259a546bc43ab9f66
Will <wveatchw@gmail.com>
2022-03-04 16:31:40 -0500

    gpu bus ids

--- hosts/asus1.nix
+++ hosts/asus1.nix
@@ -32,10 +32,10 @@
       offload.enable = true;
     
       # Bus ID of the Intel GPU. You can find it using lspci, either under 3D or VGA
-      intelBusId = "PCI:0:2:0";
+      intelBusId = "PCI:0:02:0";
     
       # Bus ID of the NVIDIA GPU. You can find it using lspci, either under 3D or VGA
-      nvidiaBusId = "PCI:1:0:0";
+      nvidiaBusId = "PCI:1:00:0";
     };
 
   ###########################
