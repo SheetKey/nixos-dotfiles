a8ceb78a5c967904fac1f1607475ac4e7b42a8f2
Will <wveatchw@gmail.com>
2022-05-27 19:28:10 -0400

    comment

--- configuration.nix
+++ configuration.nix
@@ -32,7 +32,7 @@
   # boot.loader.efi.efiSysMountPoint = "/boot/efi";
   # Define on which hard drive you want to install Grub.
   # boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
-
+#################################################################################
 
   # Set up locales.
   time.timeZone = "America/New_York";
