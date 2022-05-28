eac9685037c46acd6982ff74cb0107ed808dc382
Will <wveatchw@gmail.com>
2022-05-28 14:57:08 -0400

    remove bad lines

--- flake.nix
+++ flake.nix
@@ -31,15 +31,15 @@
 
     system = "x86_64-linux";
 
-    pkgs = import nixpkgs {
-      
-      inherit system;
+    #pkgs = import nixpkgs {
+    #  
+    #  inherit system;
 
-      config = {
-    	  allowUnfree = true;
-      };
+    #  config = {
+    #	  allowUnfree = true;
+    #  };
 
-    };
+    #};
 
     lib = nixpkgs.lib;
 
