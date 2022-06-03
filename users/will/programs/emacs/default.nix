e481795dd98bc188c763e31f84e91a4b314b9f2a
Will <wveatchw@gmail.com>
2022-06-02 20:43:41 -0400

    emacs func for workspace number

--- users/will/programs/emacs/default.nix
+++ users/will/programs/emacs/default.nix
@@ -188,6 +188,19 @@
           (will/kill-panel)
           (setq will/polybar-process (start-process-shell-command "polybar" nil "polybar panel")))
 
+        (defun will/exwm-workspace ()
+          (pcase exwm-workspace-current-index
+            (0 "[0] 1 2 3 4 5 6 7 8 9")
+            (1 "0 [1] 2 3 4 5 6 7 8 9")
+            (2 "0 1 [2] 3 4 5 6 7 8 9")
+            (3 "0 1 2 [3] 4 5 6 7 8 9")
+            (4 "0 1 2 3 [4] 5 6 7 8 9")
+            (5 "0 1 2 3 4 [5] 6 7 8 9")
+            (6 "0 1 2 3 4 5 [6] 7 8 9")
+            (7 "0 1 2 3 4 5 6 [7] 8 9")
+            (8 "0 1 2 3 4 5 6 7 [8] 9")
+            (9 "0 1 2 3 4 5 6 7 8 [9]")))
+
       '';
 
       # extra packages for emacs
