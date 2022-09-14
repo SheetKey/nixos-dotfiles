c85c2b22ea4a36cc39aea5be3924ed7d97868f05
Will <wveatchw@gmail.com>
2022-09-14 16:16:42 -0400

    theme

--- users/will/programs/emacs/emacs.el
+++ users/will/programs/emacs/emacs.el
@@ -129,7 +129,7 @@
     ;; Global settings
     (setq doom-themes-enable-bold t
           doom-themes-enable-italic t)
-    (load-theme 'doom-nord-aurora t)
+    (load-theme 'doom-one t)
 
     (doom-themes-visual-bell-config)
 )
