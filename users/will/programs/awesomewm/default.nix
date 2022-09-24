1f566d5994d9d34825f77f23dfa8cc67d644bfd4
Will <wveatchw@gmail.com>
2022-09-24 11:38:58 -0400

    new dir

--- users/will/programs/awesomewm/default.nix
+++ users/will/programs/awesomewm/default.nix
@@ -4,5 +4,7 @@
 
   home.file.".config/awesome/rc.lua".source = ./rc.lua;
 
-  #home.file.".config/awesome.mytheme.lua".source = ./mytheme.lua;
+  home.file.".config/awesome/mytheme.lua".source = ./mytheme.lua;
+
+  home.file.".config/awesome/default".source = ./default;
 }
