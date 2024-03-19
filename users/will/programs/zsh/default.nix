b3ccac1851547b8a56b75ec5cb47515622de2c68
Will <wveatchw@gmail.com>
2024-03-19 16:49:45 -0400

    remove vi keys from zsh

--- users/will/programs/zsh/default.nix
+++ users/will/programs/zsh/default.nix
@@ -34,36 +34,38 @@
     # The '' before ${vcs...} is to escape the ${} since this is official nix syntax.
 
     initExtra = ''
-      bindkey -M menuselect 'h' vi-backward-char
-      bindkey -M menuselect 'k' vi-up-line-or-history
-      bindkey -M menuselect 'l' vi-forward-char
-      bindkey -M menuselect 'j' vi-down-line-or-history
-
-      f () {
-        tmp="$(mktemp)"
-	      lf -last-dir-path="$tmp" "$@"
-	      if [ -f "$tmp" ]; then
-	      dir="$(cat "$tmp")"
-	      rm -f "$tmp" >/dev/null
-	      [ -d "$dir" ] && [ "$dir" != "($pwd)" ] && cd "$dir"
-	      fi
-      }
-
-      export XDG_DATA_HOME="$HOME/.local/share"
-
     '';
 
-    plugins = [
-      {
-        name = "zsh-vi-mode";
-	      src = pkgs.fetchFromGitHub {
-          owner = "jeffreytse";
-	        repo = "zsh-vi-mode";
-	        rev = "0eb3c7b43b1f0a81af3676b150747b839e17c125";
-	        sha256 = "PmfzWj0MynNvDaws8K0QsLcCyJULwxUyyNIjsA7oLgM=";
-	      };
-      }
-    ];
+    # initExtra = ''
+    #   bindkey -M menuselect 'h' vi-backward-char
+    #   bindkey -M menuselect 'k' vi-up-line-or-history
+    #   bindkey -M menuselect 'l' vi-forward-char
+    #   bindkey -M menuselect 'j' vi-down-line-or-history
+
+    #   f () {
+    #     tmp="$(mktemp)"
+	  #     lf -last-dir-path="$tmp" "$@"
+	  #     if [ -f "$tmp" ]; then
+	  #     dir="$(cat "$tmp")"
+	  #     rm -f "$tmp" >/dev/null
+	  #     [ -d "$dir" ] && [ "$dir" != "($pwd)" ] && cd "$dir"
+	  #     fi
+    #   }
+
+    #   export XDG_DATA_HOME="$HOME/.local/share"
+
+    # '';
+    # plugins = [
+    #   {
+    #     name = "zsh-vi-mode";
+	  #     src = pkgs.fetchFromGitHub {
+    #       owner = "jeffreytse";
+	  #       repo = "zsh-vi-mode";
+	  #       rev = "0eb3c7b43b1f0a81af3676b150747b839e17c125";
+	  #       sha256 = "PmfzWj0MynNvDaws8K0QsLcCyJULwxUyyNIjsA7oLgM=";
+	  #     };
+    #   }
+    # ];
     
   };
 
