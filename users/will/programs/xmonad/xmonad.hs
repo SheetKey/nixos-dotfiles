c9d7df2f9c21846d095f8559975e1a638211c5af
Will <wveatchw@gmail.com>
2024-03-19 13:59:43 -0400

    dzen2 status bars

--- users/will/programs/xmonad/xmonad.hs
+++ users/will/programs/xmonad/xmonad.hs
@@ -11,22 +11,33 @@ import XMonad.Hooks.EwmhDesktops (ewmhFullscreen, ewmh)
 import XMonad.Util.EZConfig (mkKeymap)
 import XMonad.Util.SpawnOnce (spawnOnce)
 import XMonad.Util.Ungrab (unGrab)
+import XMonad.Prompt.Shell (split)
+
+-- layouts
+
 
 -- layouts modifiers
 import XMonad.Layout.MultiToggle (Toggle(..),  mkToggle, single)
 import XMonad.Layout.MultiToggle.Instances (StdTransformers(NBFULL, MIRROR))
-import XMonad.Layout.NoBorders (smartBorders)
 
 -- status bar (using dzen2)
 import XMonad.Hooks.StatusBar
 import XMonad.Hooks.StatusBar.PP
 import XMonad.Hooks.ManageDocks (docks, avoidStruts, ToggleStruts(ToggleStruts))
+import XMonad.Util.Loggers
 
 -- containers
 import qualified Data.Map as M
 
 -- process
 import System.Process (shell, readCreateProcess)
+
+-- time
+import Data.Time (formatTime
+                 , defaultTimeLocale
+                 , addUTCTime
+                 , secondsToNominalDiffTime
+                 , getCurrentTime)
 
 -- base
 import System.Exit (exitSuccess)
@@ -40,20 +51,21 @@ main = do
     def
     { modMask = mod4Mask
     , keys = myKeys
-    , layoutHook = avoidStruts myLayout
+    , layoutHook = myLayout
     , terminal = "alacritty"
     , startupHook = myStartupHook
     , focusFollowsMouse = False
-    , clickJustFocuses = False
+    , clickJustFocuses = True
+    , focusedBorderColor = "#ffffff"
+    , normalBorderColor = "#000000"
     }
     
 myKeys :: XConfig Layout -> M.Map (KeyMask, KeySym) (X ())
 myKeys c = mkKeymap c $
   [ ("M-<Return>", spawn $ terminal c)
   , ("M-S-c", kill)
-  , ("M-x M-c", kill)
-  , ("M-f", spawn "rofi -show run")
-  , ("M-b", spawn "brave")
+  , ("M-x", spawn "rofi -show run")
+  , ("M-S-b", spawn "brave")
   , ("M-S-s", unGrab *> spawn "spectacle -r")
   , ("M-e", spawn "emacsclient -c")
     
@@ -65,13 +77,13 @@ myKeys c = mkKeymap c $
   , ("M-S-<Return>", windows W.swapMaster)
   , ("M-S-n", windows W.swapDown)
   , ("M-S-p", windows W.swapUp)
-  , ("M-S-f", sendMessage Shrink)
-  , ("M-S-b", sendMessage Expand)
-  , ("M-t", withFocused $ windows . W.sink)
+  -- resize
+  , ("M-f", sendMessage Expand)
+  , ("M-b", sendMessage Shrink)
   -- version issues
   -- , ("M-<Space>", withFocused toggleFullFloat)
   , ("M-<Space>", sendMessage (Toggle NBFULL) >> sendMessage ToggleStruts)
-  , ("M-x m", sendMessage (Toggle MIRROR))
+  , ("M-l m", sendMessage (Toggle MIRROR))
   
   , ("M-S-q", io exitSuccess)
   ]
@@ -89,30 +101,106 @@ myLayout = transformLayout $ defTiled
     defTiled = Tall 1 (3/100) (1/2)
 
 transformLayout = id
-  . smartBorders
+  . avoidStruts
   . mkToggle (single NBFULL)
   . mkToggle (single MIRROR)
 
 myStartupHook :: X ()
 myStartupHook = do
-  spawnOnce "trayer --edge top --align right --SetDockType true \
-            \--SetPartialStrut true --expand true --width 10 \
-            \--transparent true --tint 0x5f5f5f --height 20"
+  spawnOnce "trayer \
+            \--edge top \
+            \--align right \
+            \--widthtype request \
+            \--SetDockType true \
+            \--SetPartialStrut true \
+            \--expand true \
+            \--transparent true \
+            \--alpha 0 \
+            \--tint 0x535353 \
+            \--height 22 \
+            \--iconspacing 4 \
+            \&"
   spawnOnce "feh --bg-fill --no-fehbg \
             \~/dotfiles/nixos-dotfiles/wallpaper/green-aesthetic-wallpaper-20.jpg"
   spawnOnce "picom &"
   spawnOnce "pa-applet &"
   spawnOnce "nm-applet &"
   spawnOnce "cbatticon &"
+  spawnOnce "dzen2-nix-icon&"
   spawnOnce "emacs --daemon &"
   -- spawnOnce "xfce4-power-manager &"
-
+  
 -- dzen status bar
 myStatusBars :: ScreenId -> IO StatusBarConfig
 myStatusBars _ = do
   -- trayerWidth <- readCreateProcess
   --   (shell "xprop -name panel | grep 'program specified minimum size' | cut -d ' ' -f 5") ""
-  statusBarPipe (dzenCmd "1728") (pure dzenPP)
+  lBar <- statusBarPipe dzenCmdLeft (pure dzenLeftPP)
+  rBar <- statusBarPipe dzenCmdRight (pure dzenRightPP)
+  return $ lBar <> rBar
 
-dzenCmd :: String -> String
-dzenCmd width = "dzen2 -dock -ta l -fn \"-misc-firacode nerd font mono-medium-r-normal--0-12-0-0-m-0-iso10646-1\" -w " ++ width
+dzenCmdLeft :: String
+dzenCmdLeft = "dzen2 \
+              \-dock \
+              \-ta l \
+              \-bg '#000000' \
+              \-fg '#ffffff' \
+              \-fn '-*-firacode nerd font mono-bold-*-*-*-*-90-*-*-*-*-iso8859-1' \
+              \-h 22 \
+              \-w 1170 \
+              \-x 30 \
+              \-e 'button2=;'"
+
+dzenCmdRight :: String
+dzenCmdRight = "dzen2 \
+               \-dock \
+               \-ta r \
+               \-bg '#000000' \
+               \-fg '#ffffff' \
+               \-fn '-*-firacode nerd font mono-bold-*-*-*-*-90-*-*-*-*-iso8859-1' \
+               \-h 22 \
+               \-w 662 \
+               \-x 1200 \
+               \-e 'button2=;'"
+
+filterWindowTitle :: String -> String
+filterWindowTitle = last . split '-'
+
+dzenLeftPP :: PP
+dzenLeftPP = def
+  { ppCurrent = dzenColor "#ffffff" "#535353" . pad
+  , ppVisible = dzenColor "#ffffff" "#000000" . pad
+  , ppHidden = dzenColor "#ffffff" "#000000" . pad
+  , ppHiddenNoWindows = const ""
+  , ppUrgent = dzenColor "#ffffff" "#ff5f59" . pad
+  , ppWsSep = ""
+  , ppSep = ""
+  , ppTitle = const ""
+  , ppTitleSanitize = const ""
+  , ppLayout = pad
+  , ppOrder = \ (ws : l : _ : ts : _) -> [l, ws, ts]
+  , ppExtras =
+    [ logTitles' $ TitlesFormat
+      { focusedFormat = dzenColor "#ffffff" "#535353" 
+                        . pad . dzenEscape . filterWindowTitle
+      , unfocusedFormat = dzenColor "#ffffff" "#000000" 
+                          . pad . dzenEscape . filterWindowTitle
+      , urgentFormat = dzenColor "#ffffff" "#ff5f59" 
+                       . pad . dzenEscape . wrap "!" "!" . drop 2 . dropWhile (/= '-')
+      }
+    ]
+  }
+
+dzenRightPP :: PP
+dzenRightPP = def
+  { ppOrder = \ (_ : _ : _ : d : _) -> [d]
+  , ppExtras =
+    [ dzenColorL "#ffffff" "#000000" $ padL myDateLogger
+    ]
+  }
+
+myDateLogger :: Logger
+myDateLogger = io $ Just
+  . formatTime defaultTimeLocale "%a %b %d, %I:%M%P"
+  . addUTCTime (secondsToNominalDiffTime (-14400))
+  <$> getCurrentTime
